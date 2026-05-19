4161a28ea84ec69db62c7246969047a96aeff12b
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-05-19 17:50:45 +0700

    Implement derived classes for 'seseorang'
    Added two derived classes 'joko' and 'lia' implementing the 'pesan' method.

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -10,3 +10,21 @@ public:
     // }
 };
 
+class joko :public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "pesan dari joko" <<endl;
+    }
+};
+
+class lia :public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "pesan dari lia" <<endl;
+    }
+};
+
